ea6a9018e1b6d811d9046b7a012b6b1b8687149f
ocanosoup <ocanosoup@gmail.com>
2017-12-20 15:09:12 +0000

    Working solution without .reverse() and travis edit

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -1,7 +1,31 @@
 package quiz;
 
+import java.util.*;
+
 public class ReverseString {
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+      //Going to swap the characters
+      if(input == null) return null;
+      char[] workingCharArray = input.toCharArray();
+
+      //Pointers for the current indices to be swapped
+      int head = 0;
+      int tail = workingCharArray.length -1;
+
+      //Temporary char to hold the inbetween value that would normally get lost during the swap
+      char tempStorageChar;
+
+      //Using tail >= head condition to swap stuff up until the halfway point of the string
+      while(tail >= head) {
+
+        tempStorageChar = workingCharArray[head];
+        workingCharArray[head] = workingCharArray[tail];
+        workingCharArray[tail] = tempStorageChar;
+
+        //Moving the indices further
+        head++;
+        tail--;     
+      }
+    return new String(workingCharArray);
     }
 }
